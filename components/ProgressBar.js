647b44e742de8c7640db0aae049233422f6b6066
Samaresh Das <samareshmail679@gmail.com>
2023-04-28 16:16:02 +0530

    fixed import bug

--- components/ProgressBar.js
+++ components/ProgressBar.js
@@ -4,7 +4,7 @@ import { RxPerson } from "react-icons/rx";
 import { BsCurrencyDollar } from "react-icons/bs";
 import { AiOutlineLike } from "react-icons/ai";
 import { useSelector } from "react-redux";
-import Hr from "./shared/hr";
+import Hr from "./shared/Hr";
 import Steps from "./shared/steps";
 
 const ProgressBar = () => {
